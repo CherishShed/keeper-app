5f3f8eb530bc7181d90f66cfeec1c22a4c39dc78
Cherish C. Shed <c.shed2000@gmail.com>
2023-06-14 16:48:11 +0100

    sarted login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home"
 import Footer from './components/Footer';
+import Login from './components/Login';
+
 
 
 
@@ -14,7 +16,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path="/footer" element={<Footer />} />
-        <Route exact path="contact" />
+        <Route exact path="/login" element={<Login />} />
         <Route exact path="*" />
       </Routes>
     </BrowserRouter>
---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function Login() {
+    return (
+
+        <div className="loginForm">
+            <h1>Hello There</h1>
+        </div>
+
+    )
+}
+
+export default Login;
