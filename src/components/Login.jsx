a702cce00e4b2d75f0376f03c5e2865832fe83ea
Cherish C. Shed <c.shed2000@gmail.com>
2023-06-19 09:25:33 +0100

    fixed state for form on login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,7 +8,8 @@ import { IconButton } from '@mui/material';
 import { VisibilityOffOutlined, VisibilityOutlined } from "@mui/icons-material"
 function Login() {
     const [showPassword, setShowPassword] = useState(false);
-    const [password, setPassword] = useState("");
+    const [formData, setFormData] = useState({ username: "", password: "" });
+    const [helpText, setHelpText] = useState("")
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     const handleMouseDownPassword = (event) => {
@@ -16,23 +17,24 @@ function Login() {
     };
 
     function handleButton(ev) {
-        const passLength = password.length;
+        const passLength = formData.password.length;
+        const usernameLength = formData.username.length;
         const offset = ev.target.offsetLeft;
         console.log(passLength, offset)
         let displace = "0px";
-        if (passLength < 8) {
+        if (passLength < 8 || usernameLength < 8) {
             if (offset <= 98) {
                 displace = "100px";
             } else if (offset > 96) {
                 displace = "-100px";
             }
+            ev.target.setAttribute("disabled", true)
         } else {
-            // ev.target.setAttribute("disabled", false)
+            ev.target.removeAttribute("disabled")
             ev.target.style.background = "green"
         }
         ev.target.style.left = displace;
     }
-
     return (
 
         <div className="loginForm">
@@ -44,11 +46,20 @@ function Login() {
                         <img alt="logo" src="Screenshot_2023-06-15_113137-removebg-preview.png" className='logo' />
                         <p>Welcome To Your Note Assistant</p>
                     </div>
-                    <TextField variant='standard' label="Email" type='email' color='warning' required InputProps={{ startAdornment: <InputAdornment position="start"><AccountCircleIcon /></InputAdornment> }} className='form-input-field' />
-                    <TextField onChange={(e) => {
+                    <TextField helperText={helpText} variant='standard' label="Email" type='email' color='warning' required InputProps={{ startAdornment: <InputAdornment position="start"><AccountCircleIcon /></InputAdornment> }} className='form-input-field'
+                        onChange={(e) => {
+                            document.querySelector(".loginButton").style.left = "0px";
+                            document.querySelector(".loginButton").removeAttribute("disabled");
+                            setFormData({ ...formData, username: e.target.value })
+                        }}
+                        value={formData.username}
+                        key={1}
+                    />
+                    <TextField key={2} helperText={helpText} onChange={(e) => {
                         document.querySelector(".loginButton").style.left = "0px";
-                        setPassword(e.target.value)
-                    }} value={password} variant='standard' label="Password" type={showPassword ? 'text' : "password"} color="warning" required InputProps={{
+                        document.querySelector(".loginButton").removeAttribute("disabled");
+                        setFormData({ ...formData, password: e.target.value })
+                    }} value={formData.password} variant='standard' label="Password" type={showPassword ? 'text' : "password"} color="warning" required InputProps={{
                         startAdornment: <InputAdornment position="start"><EnhancedEncryptionIcon /></InputAdornment>, endAdornment:
                             <InputAdornment position="end" >
                                 <IconButton
