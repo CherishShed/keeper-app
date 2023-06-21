497d85fef284d292263fdd6b4c9ce7cc4fc3cd79
Cherish C. Shed <c.shed2000@gmail.com>
2023-06-21 14:23:05 +0100

    consumed post api for form

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -13,14 +13,15 @@ import "react-toastify/dist/ReactToastify.css";
 
 function DetailsForm() {
     const [userData, setUserData] = useState({ firstName: "", lastName: "", username: "" })
-
+    const [toastText, settoastText] = useState("")
 
     function picChange(event) {
         if (event.target.files.length > 0) {
             const file = event.target.files[0];
+            // setUserData({ ...userData, profilePic: file });
             const reader = new FileReader();
             reader.onload = function () {
-                console.log(reader.result);
+                // console.log(reader.result);
                 document.getElementsByClassName(styles["profile-image"])[0].setAttribute("src", reader.result);
             }
 
@@ -39,9 +40,46 @@ function DetailsForm() {
                 window.location.pathname = "/login"
             })
     }, [])
+    useEffect(() => {
+        if (toastText === "Error Occured") {
+            toast.error(toastText, {
+                position: toast.POSITION.TOP_RIGHT,
+                className: 'foo-bar',
+                pauseOnHover: false,
+                autoClose: 1000,
+                theme: "light"
+            });
+        } else if (toastText === "Successful") {
+            toast.success(toastText, {
+                position: toast.POSITION.TOP_RIGHT,
+                className: 'foo-bar',
+                pauseOnHover: false,
+                autoClose: 1000,
+                theme: "light"
+            });
+        }
+        else {
+            return;
+        }
+    }, [toastText])
+    function handleSubmit(event) {
+        console.log("here");
+        event.preventDefault();
+        axios.post("http://localhost:8081/userDetails", event.target, { headers: { Authorization: localStorage.getItem("token") } })
+            .then((result) => {
+                console.log(result);
+                settoastText(result.data.message)
+                setTimeout(() => {
+                    if (result.data.success) {
+                        window.location.pathname = "/"
+                    }
+                }, 1000)
+            })
+    }
     return (
         <div className={styles["details-container"]}>
-            <FormControl method="POST" encType="multipart/form-data" className={styles["details-form"]}>
+            <ToastContainer />
+            <FormControl component="form" className={styles["details-form"]} onSubmit={(e) => handleSubmit(e)}>
                 <img alt="logo" src="Screenshot_2023-06-15_113137-removebg-preview.png" className='logo' />
                 <div className={styles["pic-container"]}>
                     <div className={styles["profile-image-container"]}>
@@ -54,9 +92,20 @@ function DetailsForm() {
                         variant="contained" onClick={(e) => document.getElementById('picfileInput').click()}>Change Picture <CloudUpload /></Button>
                 </div>
                 <div>
-                    <TextField name="fname" label="" placeholder='fname' id="fname" required variant='outlined' color='warning' style={{ width: "50%" }} value={userData.firstName} />
-                    <TextField name="lname" label="Last Name" id="lname" required variant='outlined' color='warning' style={{ width: "50%" }} value={userData.lastName} /></div>
-                <TextField type="email" name="email" id="emailaddress" label="Email" variant='filled' fullWidth value={userData.username} InputProps={{ startAdornment: <InputAdornment position='start'><AccountCircleIcon /></InputAdornment> }} />
+                    <TextField name="firstName" label="First Name" id="fname" required variant='outlined' color='warning' style={{ width: "50%" }} value={userData.firstName}
+                        onChange={(e) => {
+                            setUserData({ ...userData, firstName: e.target.value })
+                        }}
+
+                    />
+                    <TextField name="lastName" label="Last Name" id="lname" required variant='outlined' color='warning' style={{ width: "50%" }} value={userData.lastName}
+                        onChange={(e) => {
+                            setUserData({ ...userData, lastName: e.target.value })
+                        }}
+                    />
+                </div>
+                <TextField type="email" name="username" id="emailaddress" label="Email" variant='filled' fullWidth value={userData.username} InputProps={{ startAdornment: <InputAdornment position='start'><AccountCircleIcon /></InputAdornment> }}
+                    disabled />
                 <Button type="submit" size="large" variant="contained" color='success' className={styles["submit-button"]}>Submit</Button>
 
             </FormControl>
