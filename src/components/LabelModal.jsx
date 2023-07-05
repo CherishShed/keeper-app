49445dd93e919119e35534557534b5413b0d7bce
¨DuyileS¨ <duyile35@gmail.com>
2023-07-05 07:36:40 +0100

    started designing edit label modal

--- src/components/LabelModal.jsx
+++ src/components/LabelModal.jsx
@@ -30,7 +30,7 @@ export default function AddLabelModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography id="modal-modal-title" variant="subtitle1" component="h2">
             Edit labels
           </Typography>
           <TextField variant="standard" placeholder="Create new Label" />
