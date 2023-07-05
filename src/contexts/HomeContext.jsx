4cb288c7286655a89c93ddd0ee80e21ad0e523d8
¨DuyileS¨ <duyile35@gmail.com>
2023-07-05 06:46:41 +0100

    created modal context

--- src/contexts/HomeContext.jsx
+++ src/contexts/HomeContext.jsx
@@ -2,3 +2,14 @@ import React, { createContext, useState, useReducer } from "react";
 
 export const LabelModal = createContext();
 export const TextColor = createContext();
+
+export function LabelModalContextProvider(props) {
+
+    const [modalOpen, setModalOpen] = useState(true); 
+
+    return (
+        <LabelModal.Provider value={{ modalOpen, setModalOpen }}>
+            {props.children}
+        </LabelModal.Provider>
+    )
+}
